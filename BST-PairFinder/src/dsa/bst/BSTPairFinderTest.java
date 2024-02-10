5c91e27e1a83cdce7e00ca791909b8c5b01773ae
pratibhadixit549 <pratibhadixit549@gmail.com>
2024-02-10 14:57:48 +0530

    BST-PairFinder project updated

--- BST-PairFinder/src/dsa/bst/BSTPairFinderTest.java
+++ BST-PairFinder/src/dsa/bst/BSTPairFinderTest.java
@@ -6,12 +6,10 @@ public class BSTPairFinderTest {
 
 		BinarySearchTree bst = BSTUtils.sampleBST();
 
-		BSTPairFinder pairFinder = 
-				// new BSTPairFinder(bst, 120);
-				new BSTPairFinder(bst, 129);
-		
-		pairFinder.findPairs();
+		BSTPairFinder pairFinder = new BSTPairFinder(bst, 120); // Sum found
+		// new BSTPairFinder(bst, 129); // Nodes not found
 
+		pairFinder.findPairs();
 
 		// findPairs()
 
